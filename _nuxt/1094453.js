ec41ab779ecc894480e43f23818fd9ac51cae05d
eren0521 <82822425+eren0521@users.noreply.github.com>
2025-11-03 09:46:14 +0900

    Add files via upload

---
+++ _nuxt/1094453.js
@@ -0,0 +1,200 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [24],
+  {
+    392: function (t, e, i) {
+      var p = {
+        "./1-2.png": 393,
+        "./5-6.png": 394,
+        "./7-8.png": 395,
+        "./9-10.png": 396,
+      };
+      function n(t) {
+        var e = h(t);
+        return i(e);
+      }
+      function h(t) {
+        if (!i.o(p, t)) {
+          var e = new Error("Cannot find module '" + t + "'");
+          throw ((e.code = "MODULE_NOT_FOUND"), e);
+        }
+        return p[t];
+      }
+      (n.keys = function () {
+        return Object.keys(p);
+      }),
+        (n.resolve = h),
+        (t.exports = n),
+        (n.id = 392);
+    },
+    393: function (t, e, i) {
+      t.exports = {
+        srcSet:
+          i.p +
+          "img/9a97478-640.webp 640w," +
+          i.p +
+          "img/20d2162-1024.webp 1024w," +
+          i.p +
+          "img/bb7b387-1110.webp 1110w",
+        images: [
+          { path: i.p + "img/9a97478-640.webp", width: 640, height: 452 },
+          { path: i.p + "img/20d2162-1024.webp", width: 1024, height: 723 },
+          { path: i.p + "img/bb7b387-1110.webp", width: 1110, height: 784 },
+        ],
+        src: i.p + "img/9a97478-640.webp",
+        toString: function () {
+          return i.p + "img/9a97478-640.webp";
+        },
+        placeholder:
+          "data:image/webp;base64,UklGRhYBAABXRUJQVlA4IAoBAABwBgCdASooABwAPpE8mUkloyIhMBVYALASCWcAyNXdI4OyU2rtZR2UEhLPqUdVjuRUZSrhi82jpTnxQoAA/vl0QzuQ+x/34sfhusj/d4tO1mVJRB9TMdsZ2tfbndjC9oIz+DA77PGxTYXf+o4EU9y7b3EUoBH7p8+NGehugxFUioabi8n2VbGtkDzt3ksPCvBmH4cnFyf7rPnOpK/R0kuybDglSKa5WXXO67MkfrRlGq7v28AR6zVxaQ4mont4QT/s326z9f3/QxbEfXUcvWdYoQM4hcEtf6464evZ3fGkRJZke4gC6moR1C3FoiEnVD0Rltl6f/3aJWyl0YTYrNKqPA3BC1TlADgAAA==",
+        width: 640,
+        height: 452,
+      };
+    },
+    394: function (t, e, i) {
+      t.exports = {
+        srcSet:
+          i.p +
+          "img/8ba8692-640.webp 640w," +
+          i.p +
+          "img/0ec1e59-1024.webp 1024w," +
+          i.p +
+          "img/9010f4d-1110.webp 1110w",
+        images: [
+          { path: i.p + "img/8ba8692-640.webp", width: 640, height: 457 },
+          { path: i.p + "img/0ec1e59-1024.webp", width: 1024, height: 731 },
+          { path: i.p + "img/9010f4d-1110.webp", width: 1110, height: 792 },
+        ],
+        src: i.p + "img/8ba8692-640.webp",
+        toString: function () {
+          return i.p + "img/8ba8692-640.webp";
+        },
+        placeholder:
+          "data:image/webp;base64,UklGRhgBAABXRUJQVlA4IAwBAABQBgCdASooAB0APpE8lkiloyIhMBVaqLASCWcAzjmLIkjfGaNNah+IzyLriVBPvDhsP6MJovIZfop8AAD++XMj1OVa2sd9+zpCrx/sF8vlUHmGJH/coZTL3BuO2rQBDPcXijz48mfWfDX2oz1cntVL7oj4uZrA+z0XN9tmIzIuj7SBvf4zN4zSRiBWHjD2//xsHvV+SZZ4hmy03rfRNB3gyI44vnvO8meje8nvfws8oSUkAZxIdb3m51VJ16cu/cs3myUEZ4N3uqhSL0panvjpnijnh2o6roSfdEDbUP2UMrK0C5gQyC/9WAE3LkoidQVuU8hRuYglRGI6vLFEmPG6yodxr+6Irm14AAAA",
+        width: 640,
+        height: 457,
+      };
+    },
+    395: function (t, e, i) {
+      t.exports = {
+        srcSet:
+          i.p +
+          "img/dcd0b92-640.webp 640w," +
+          i.p +
+          "img/89750d7-1024.webp 1024w," +
+          i.p +
+          "img/9d57738-1110.webp 1110w",
+        images: [
+          { path: i.p + "img/dcd0b92-640.webp", width: 640, height: 457 },
+          { path: i.p + "img/89750d7-1024.webp", width: 1024, height: 731 },
+          { path: i.p + "img/9d57738-1110.webp", width: 1110, height: 792 },
+        ],
+        src: i.p + "img/dcd0b92-640.webp",
+        toString: function () {
+          return i.p + "img/dcd0b92-640.webp";
+        },
+        placeholder:
+          "data:image/webp;base64,UklGRlYBAABXRUJQVlA4IEoBAAAwBwCdASooAB0APpFAmkklo6IhKrgKALASCWUAyhmgJYA3ksiHAgQFdZJ2pQfVhiWkm9+74cSjjVT4tTqaPX8en4AA/vl0sQ2lx01ZvcQ0qyNIybvDTujcbD0qmM788mbj3fp8XvMHieUR+ytLy8YMXDTYIiq+Nlzp44zRGHw6taNbnYcC80Ta8lFSmb2nxsgKZMkU2GHUEAxZAV7RMu5sbFukssWAXt/7cmBlhutg5b7Mn+DOX43Vu67kMwd4O+qFKyN24RlTkHr9C/FG1VbZAX32MPyKNqNU/a+klxNGk8T/fUw2I5sRGkaUciGnEb4g03fUZk8xZ9R731GUKnxkpbO03nIcy6qmea9sp9vmhKqNYL219cJCzoQkEArz/27w4Lx+QLXqSR83bjgoBYXhxC7iXZlURuG9YqKO88ojiMWAc84T4wAAAAA=",
+        width: 640,
+        height: 457,
+      };
+    },
+    396: function (t, e, i) {
+      t.exports = {
+        srcSet:
+          i.p +
+          "img/efb5b30-640.webp 640w," +
+          i.p +
+          "img/af1de2a-1024.webp 1024w," +
+          i.p +
+          "img/01be6f7-1110.webp 1110w",
+        images: [
+          { path: i.p + "img/efb5b30-640.webp", width: 640, height: 457 },
+          { path: i.p + "img/af1de2a-1024.webp", width: 1024, height: 731 },
+          { path: i.p + "img/01be6f7-1110.webp", width: 1110, height: 792 },
+        ],
+        src: i.p + "img/efb5b30-640.webp",
+        toString: function () {
+          return i.p + "img/efb5b30-640.webp";
+        },
+        placeholder:
+          "data:image/webp;base64,UklGRgYBAABXRUJQVlA4IPoAAADQBQCdASooAB0APpE4mEiloyIhMBqoALASCWcAzf2LcoOtEmQKIfWD4q3nEQt7Tm0QEsQCl4eAAP76G3rb0esMm0J8U52/VgADEc9OhtY+e05P87a6b7L4UMrP1BU2WqbY8TEnDw49vZZwoF8ofIbxjXHVfK0zM7RzF8bNVMEIklv6orVuO6t7YiT/PmrK2WcMSiNebamDiOj1naPoG9p/YCKqtXxEVe9fT33C1oKfoFbDCNmmGZ6eLEsdYtNfeQFN6YNYDhhl2c/a0mlX3udnj4RKe1lfZPKZ1ZN6L5dTqaK4WlHfnMnyckrQKYJDo/3WhHyL3zv5AAAA",
+        width: 640,
+        height: 457,
+      };
+    },
+    514: function (t, e, i) {
+      "use strict";
+      i.r(e);
+      i(12), i(21), i(22), i(33), i(27), i(66);
+      var p = {
+          name: "ClientsLogo",
+          components: {
+            LazyImage: function () {
+              return i.e(0).then(i.bind(null, 513));
+            },
+          },
+          data: function () {
+            return { clients: [] };
+          },
+          mounted: function () {
+            this.importAll(i(392));
+          },
+          methods: {
+            importAll: function (t) {
+              var e = this;
+              t.keys().forEach(function (i) {
+                e.clients.push({
+                  pathLong: t(i),
+                  pathShort: i,
+                  alt: i.replace(/((\.\/)|\.[a-z]+)/g, ""),
+                });
+              });
+            },
+          },
+        },
+        n = i(36),
+        h = Object(n.a)(
+          p,
+          function () {
+            var t = this,
+              e = t._self._c;
+            return e(
+              "b-row",
+              {
+                staticClass: "d-grid d-grid--gap-25",
+                staticStyle: { "max-width": "100%" },
+              },
+              t._l(t.clients, function (i) {
+                return e(
+                  "figure",
+                  { key: i.idx, staticStyle: { "min-width": "400px" } },
+                  [
+                    e("lazy-image", {
+                      attrs: {
+                        alt: i.alt,
+                        src: i.pathLong,
+                        width: i.pathLong.width,
+                        height: i.pathLong.height,
+                      },
+                    }),
+                    t._v(" "),
+                    e("figcaption", { staticClass: "sr-only" }, [
+                      t._v(t._s(i.alt)),
+                    ]),
+                  ],
+                  1
+                );
+              }),
+              0
+            );
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        );
+      e.default = h.exports;
+    },
+  },
+]);
